386071fe962cc6e7a79c09365d126c3695aeb4a1
AlexeyKop <a.kop@bk.ru>
2023-11-24 20:46:23 +0400

    studied working with arrays

---
+++ Test014_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿
+void FillArray(int[] collection) // метод для наполнения массива
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) // метод для вывода на печать массива
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collect, int find)  // метод для нахождения индекса искомого элемента в массиве
+{
+    int count = collect.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collect[index] == find)
+        {
+            position = index;
+            break;  // прерываем цикл при первом нахождении искомого аргумента
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
